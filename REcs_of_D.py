10fa8d3a7c3620dcc1c464789390970b2b0ce2f4
Emre-the-developer <alacaemre49@gmail.com>
2024-07-07 19:57:58 +0200

    homepage

--- REcs_of_D.py
+++ REcs_of_D.py
@@ -16,8 +16,55 @@ st.markdown("""
         color: #000000;
         width: 100%;
     }
+    .top-picks-container {
+        background-color: #CC5500;
+        padding: 10px;
+        border-radius: 5px;
+    }
+    .top-pick {
+        background-color: #333333;
+        color: #FFFFFF;
+        margin: 5px;
+        padding: 10px;
+        border-radius: 5px;
+    }
+    .search-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 20px;
+    }
+    .search-input {
+        width: 50%;
+        padding: 10px;
+        border-radius: 5px;
+        border: none;
+    }
     </style>
     """, unsafe_allow_html=True)
+
+# Function to show the home page
+def show_home_page(user_name):
+    st.markdown(f"""
+        <div style="background-color: #CC5500; padding: 10px; display: flex; justify-content: space-between; align-items: center;">
+            <div>{user_name}</div>
+            <div>Mylist</div>
+            <div>Sign Out</div>
+        </div>
+        <div class="search-container">
+            <input type="text" class="search-input" placeholder="Search">
+        </div>
+        <h2 style="color: #FFFFFF;">Top Picks</h2>
+        <div class="top-picks-container">
+            <div class="top-pick">
+                <div>Anime Name</div>
+                <div>TV/Movie 26 Episodes</div>
+                <div style="background-color: #FF0000; height: 150px; width: 100px;"></div>
+                <div>Score: 7.0</div>
+                <div>Genre: Western</div>
+            </div>
+        </div>
+        """, unsafe_allow_html=True)
 
 if "page" not in st.session_state:
     st.session_state.page = "Sign In"
@@ -28,6 +75,8 @@ def show_sign_in():
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
     if st.button("Sign In"):
+        st.session_state.page = "Home"
+        st.session_state.user_name = email
         st.success(f"Welcome, {email}!")
     if st.button("Create Account"):
         st.session_state.page = "Sign Up"
@@ -41,6 +90,8 @@ def show_sign_up():
     terms = st.checkbox("I accept the Terms of Service and Privacy Policy")
     if st.button("Create Account"):
         if terms:
+            st.session_state.page = "Home"
+            st.session_state.user_name = username
             st.success(f"Account created for {username}!")
         else:
             st.error("You must accept the terms to create an account.")
@@ -49,5 +100,8 @@ def show_sign_up():
 
 if st.session_state.page == "Sign In":
     show_sign_in()
-else:
+elif st.session_state.page == "Sign Up":
     show_sign_up()
+else:
+    show_home_page(st.session_state.get("user_name", "User"))
+
