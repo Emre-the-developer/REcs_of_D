cd020ab4b5bdb134b729fc1d4cc79b0bc478eecb
Emre-the-developer <alacaemre49@gmail.com>
2024-07-07 19:02:52 +0200

    sign up and Sign In made

--- REcs_of_D.py
+++ REcs_of_D.py
@@ -1,2 +1,53 @@
-from tkinter import *
-print("Merhaba ")
+import streamlit as st
+
+st.markdown("""
+    <style>
+    @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
+    .main {
+        background-color: #333333;
+        color: #FFFFFF;
+    }
+    .stTextInput input {
+        background-color: #333333;
+        color: #FFFFFF;
+    }
+    .stButton button {
+        background-color: #FF9900;
+        color: #000000;
+        width: 100%;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+if "page" not in st.session_state:
+    st.session_state.page = "Sign In"
+
+def show_sign_in():
+    st.markdown(f'<i class="fa-solid fa-lock fa-3x" style="color: #FF9900;"></i>', unsafe_allow_html=True)
+    st.title("Sign In")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    if st.button("Sign In"):
+        st.success(f"Welcome, {email}!")
+    if st.button("Create Account"):
+        st.session_state.page = "Sign Up"
+
+def show_sign_up():
+    st.markdown(f'<i class="fa-solid fa-lock fa-3x" style="color: #FF9900;"></i>', unsafe_allow_html=True)
+    st.title("Sign Up")
+    username = st.text_input("Username")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    terms = st.checkbox("I accept the Terms of Service and Privacy Policy")
+    if st.button("Create Account"):
+        if terms:
+            st.success(f"Account created for {username}!")
+        else:
+            st.error("You must accept the terms to create an account.")
+    if st.button("Sign In"):
+        st.session_state.page = "Sign In"
+
+if st.session_state.page == "Sign In":
+    show_sign_in()
+else:
+    show_sign_up()
